ca059a723dc0a735b40e591a76599cb7ab70eeaf
Greg Yip <32555833+gregyip95@users.noreply.github.com>
2018-01-10 19:46:50 -0500

    Add files via upload

--- README.md
+++ README.md
@@ -1,1 +1,18 @@
-# liri
+
+
+to install npm packages run these commands 
+
+
+npm install twitter
+npm install spotify
+npm install request
+
+*Commands to run LIRI
+
+
+
+* node liri.js my-tweets
+* node liri.js spotify-this-song '<song name here>'
+* node liri.js movie-this '<movie name here>'
+* node liri.js do-what-it-says 
+
---
+++ keys.js
@@ -0,0 +1,9 @@
+
+console.log('this is loaded');
+
+exports.twitterKeys = {
+  consumer_key: 'cnpE1P0bYEM9Ms4Bx5fNPLaKr',
+  consumer_secret: 'ySYfPxL5P2uvNjIYtyynr7Rq8D9yTSPKiECXTHe6GClC6lXqsq',
+  access_token_key: '951236501244870656-8TYZhntGbuI42D7rTnKkb6NlNy2l92m',
+  access_token_secret: 'tTAmAFUHxWBWbWzKgxJmlpcsCxnNHBDFLc3aJUlFRQEBN',
+}
